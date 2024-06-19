e659cf755b91cec0a4d7ef34feb63b592e4d9469
ThomasGaillot <thomas.gaillot@enogrid.com>
2024-06-19 17:09:19 +0200

    PRM identifier OTP responsive

--- app/form-made-with-zod/page.tsx
+++ app/form-made-with-zod/page.tsx
@@ -312,31 +312,33 @@ function ProfileForm() {
                                     </FormDescription>
                                     <FormControl>
                                         <InputOTP onChange={field.onChange} maxLength={14} pattern="^[0-9]+$">
-                                            <InputOTPGroup>
-                                                <InputOTPSlot index={0}/>
-                                                <InputOTPSlot index={1}/>
-                                                <InputOTPSlot index={2}/>
-                                                <InputOTPSlot index={3}/>
-                                            </InputOTPGroup>
-                                            <InputOTPSeparator/>
-                                            <InputOTPGroup>
-                                                <InputOTPSlot index={4}/>
-                                                <InputOTPSlot index={5}/>
-                                                <InputOTPSlot index={6}/>
-                                                <InputOTPSlot index={7}/>
-                                            </InputOTPGroup>
-                                            <InputOTPSeparator/>
-                                            <InputOTPGroup>
-                                                <InputOTPSlot index={8}/>
-                                                <InputOTPSlot index={9}/>
-                                                <InputOTPSlot index={10}/>
-                                                <InputOTPSlot index={11}/>
-                                            </InputOTPGroup>
-                                            <InputOTPSeparator/>
-                                            <InputOTPGroup>
-                                                <InputOTPSlot index={12}/>
-                                                <InputOTPSlot index={13}/>
-                                            </InputOTPGroup>
+                                            <div className="flex flex-wrap gap-2 items-center">
+                                                <InputOTPGroup>
+                                                    <InputOTPSlot index={0}/>
+                                                    <InputOTPSlot index={1}/>
+                                                    <InputOTPSlot index={2}/>
+                                                    <InputOTPSlot index={3}/>
+                                                </InputOTPGroup>
+                                                <InputOTPSeparator />
+                                                <InputOTPGroup>
+                                                    <InputOTPSlot index={4}/>
+                                                    <InputOTPSlot index={5}/>
+                                                    <InputOTPSlot index={6}/>
+                                                    <InputOTPSlot index={7}/>
+                                                </InputOTPGroup>
+                                                <InputOTPSeparator/>
+                                                <InputOTPGroup>
+                                                    <InputOTPSlot index={8}/>
+                                                    <InputOTPSlot index={9}/>
+                                                    <InputOTPSlot index={10}/>
+                                                    <InputOTPSlot index={11}/>
+                                                </InputOTPGroup>
+                                                <InputOTPSeparator/>
+                                                <InputOTPGroup>
+                                                    <InputOTPSlot index={12}/>
+                                                    <InputOTPSlot index={13}/>
+                                                </InputOTPGroup>
+                                            </div>
                                         </InputOTP>
                                     </FormControl>
                                     <FormMessage/>
