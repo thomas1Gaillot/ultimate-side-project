16901be45782de943564952ee8fca84549135d1f
ThomasGaillot <thomas.gaillot@enogrid.com>
2024-06-19 10:05:24 +0200

    submit a form easily with zod

--- app/form-made-with-zod/page.tsx
+++ app/form-made-with-zod/page.tsx
@@ -1,16 +1,21 @@
 "use client"
 import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import {z} from "zod"
-import {Controller, useForm} from "react-hook-form";
+import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {Button} from "@/components/ui/button";
 import {Input} from "@/components/ui/input";
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
-import {useToast} from "@/components/ui/use-toast";
+import {useToast} from "@/components/hooks/use-toast";
 import {IconBatteryEco, IconBuildingWindTurbine, IconRipple, IconSolarPanel} from "@tabler/icons-react";
 import {cn} from "@/lib/utils";
 import PaymentToggleButtons from "@/components/[locale]/form-made-with-zod/payment-toggle-buttons";
 import {Separator} from "@/components/ui/separator";
+import SearchAddress from "@/components/ui/search-address";
+import OperationalToggleButtons from "@/components/[locale]/form-made-with-zod/operational-toggle-buttons";
+import Link from "next/link";
+import {badgeVariants} from "@/components/ui/badge";
+import {ArrowLeftIcon} from "lucide-react";
 
 const productionTypes = ["Wind", "Solar", "Hydro", "Other"] as const;
 const segmentTypes = ["C1", "C2", "C3", "C4", "C5"] as const;
@@ -18,8 +23,8 @@ export default function FormMadeWithZod() {
     return (
         <div className="container py-24 lg:py-32">
             {/* Announcement Banner */}
-            <div className="flex justify-center">
-
+            <div className="flex flex-col items-center gap-8 justify-center">
+                <MadeWithZodBadges/>
                 <ProfileForm/>
             </div>
         </div>
@@ -33,8 +38,16 @@ const formSchema = z.object({
     minPrice: z.preprocess((val) => Number(val), z.number().min(0)).optional(),
     maxPrice: z.preprocess((val) => Number(val), z.number().min(0)).optional(),
     isPriceFree: z.boolean(),
-    segmentType : z.enum(segmentTypes),
+    segmentType: z.enum(segmentTypes, {message: 'Segment is required.'}),
     productionType: z.enum(productionTypes),
+    address : z.object(
+        {
+            label: z.string(),
+            lat: z.number(),
+            lon: z.number(),
+        },  {message: 'Address is required.'}
+    ),
+    isOperational: z.boolean(),
 });
 
 function ProfileForm() {
@@ -47,6 +60,7 @@ function ProfileForm() {
             username: "",
             name: "",
             isPriceFree: false,
+            isOperational: true,
             productionType: productionTypes[1],
         },
     });
@@ -59,13 +73,19 @@ function ProfileForm() {
             title: "Form has been sent",
             description: JSON.stringify(values),
         });
-        console.log(values);
     }
 
     return (
-        <Card className="w-full max-w-2xl">
+        <Card className="w-full max-w-2xl shadow-none">
             <CardHeader>
-                <CardTitle className="text-2xl">{"First, create your production site"}</CardTitle>
+                <CardTitle className="text-2xl flex flex-col-reverse gap-2 md:flex-row justify-between">{"First, create your production site"}
+                    <Button
+                        onClick={() => {
+                            window.location.href = "/"
+                        }}
+                        className={"w-max"}
+                        variant={"outline"}><ArrowLeftIcon className={'pr-2'}/> Back to Home </Button>
+                </CardTitle>
                 <CardDescription>
                     Fill in the below information
                 </CardDescription>
@@ -86,33 +106,8 @@ function ProfileForm() {
                                 </FormItem>
                             )}
                         />
+                        <Separator/>
                         <FormField
-                            control={form.control}
-                            name="name"
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormLabel>The production site</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Powerstation 150 kW" {...field} />
-                                    </FormControl>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="annualProduction"
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormLabel>Annual production in kWH per year (optional)</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="7 500 kWh per year" {...field} />
-                                    </FormControl>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                        />
-                        <Controller
                             control={form.control}
                             name="productionType"
                             render={({field}) => (
@@ -133,7 +128,35 @@ function ProfileForm() {
                                 </FormItem>
                             )}
                         />
-                        <Controller
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>The production site</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Powerstation 150 kW" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="annualProduction"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Annual production in kWH per year (optional)</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="7 500 kWh per year" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <FormField
                             control={form.control}
                             name="segmentType"
                             render={({field}) => (
@@ -155,7 +178,7 @@ function ProfileForm() {
                         />
                         <Separator/>
                         <CardTitle className="text-2xl">{"Pricing"}</CardTitle>
-                        <Controller
+                        <FormField
                             control={form.control}
                             name="isPriceFree"
                             render={({field}) => (
@@ -165,7 +188,7 @@ function ProfileForm() {
                         {form.watch('isPriceFree') ? (
                             <div className="mt-4">
                                 <span className="block text-sm font-medium text-gray-800 dark:text-white">
-                                    You are giving all your production to the consumers !
+                                    You are giving all your production to the consumers fo free !
                                 </span>
                             </div>
                         ) : (
@@ -206,6 +229,36 @@ function ProfileForm() {
                         )}
                         <Separator/>
                         <CardTitle className="text-2xl">{"Localisation"}</CardTitle>
+                        <FormField
+                            control={form.control}
+                            name="isOperational"
+                            render={({field}) => (
+                                <OperationalToggleButtons isOperational={field.value} onChange={field.onChange}/>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="address"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>The address of the production site</FormLabel>
+                                    <FormControl>
+                                        <div className={"w-full"}>
+                                            <SearchAddress onSelectLocation={(location) => {
+                                                const newLocation = {
+                                                    label: location?.label,
+                                                    lat: location?.x,
+                                                    lon: location?.y,
+                                                };
+                                                field.onChange(newLocation);
+                                            }}/>
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+
                         <Button type="submit">Next step : Project informations</Button>
                     </form>
                 </Form>
@@ -260,4 +313,17 @@ function getIcon(type: string) {
         default:
             return null;
     }
+}
+function MadeWithZodBadges() {
+    return (
+        <div className={"flex items-center gap-2"}>
+            <p className={' text-xs font-light text-gray-500'}>made with</p>
+            <Link target={'_blank'} href={'https://zod.dev/'}
+                  className={badgeVariants({variant: "outline"})}>zod</Link>
+            <Link target={'_blank'} href={'https://ui.shadcn.com'}
+                  className={badgeVariants({variant: "outline"})}>shadcn-ui</Link>
+            <Link target={'_blank'} href={'https://react-hook-form.com/'}
+                  className={badgeVariants({variant: "outline"})}>react-hook-form</Link>
+        </div>
+    );
 }
