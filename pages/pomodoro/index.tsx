7bd7d846f130c1c071a0a11cbec857601fef16b0
ThomasGaillot <thomas.gaillot@enogrid.com>
2024-07-01 17:55:49 +0200

    show code in tab

--- pages/pomodoro/index.tsx
+++ pages/pomodoro/index.tsx
@@ -2,7 +2,8 @@ import PomodoroWidget from "@/pages/pomodoro/PomodoroWidget";
 import {TypographyH1, TypographyH2, TypographyLead} from "@/components/ui/typography";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 import {Card} from "@/components/ui/card";
-
+import CodeBlocks from "@/components/[locale]/code-blocks";
+import pomodoroWidgetContent from "@/data/pomodoro-widget-index";
 export default function Pomodoro() {
     return (
         <div className={"flex flex-col gap-8 pb-8"}>
@@ -25,7 +26,9 @@ export default function Pomodoro() {
                         <PomodoroWidget/>
                     </Card>
                 </TabsContent>
-                <TabsContent value="code">Change your password here.</TabsContent>
+                <TabsContent value="code">
+                    <CodeBlocks codeString={pomodoroWidgetContent}/>
+                </TabsContent>
             </Tabs>
             <TypographyH2>
                 Stack
