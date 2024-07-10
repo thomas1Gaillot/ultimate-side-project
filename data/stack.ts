b1aca825bc48ce2a0b683a9a2ce4ea6fab8b0b37
ThomasGaillot <thomas.gaillot@enogrid.com>
2024-07-10 14:24:50 +0200

    stack update

--- data/stack.ts
+++ data/stack.ts
@@ -17,4 +17,22 @@ export const techStack = [
         link: 'https://ui.shadcn.com/',
         shortDescription: 'Copy & Paste components on top of radix-ui primitives. Accessible. Well designed. Enormous gain of time.'
     },
+    {
+        name: 'Zustand',
+        logo: '/logos/zustand.png',
+        link: 'https://zustand-demo.pmnd.rs/',
+        shortDescription: 'Easy to use, fast, and efficient global state management for React.'
+    },
+    {
+        name: 'Zod',
+        logo: '/logos/zod.png',
+        link: 'https://zod.dev/',
+        shortDescription: 'Easy to use, fast, and efficient form validation library for TypeScript.'
+    },
+    {
+        name: 'Prisma',
+        logo: '/logos/prisma.svg',
+        link: 'https://www.prisma.io/',
+        shortDescription: 'Easy to use, understand and maintain ORM for Node.js.'
+    },
 ];
