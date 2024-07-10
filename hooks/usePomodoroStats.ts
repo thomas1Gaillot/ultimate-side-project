d0a05068632e95eb84d211f33ab548a207f20ab6
ThomasGaillot <thomas.gaillot@enogrid.com>
2024-07-10 17:06:42 +0200

    try to fix stats

--- hooks/usePomodoroStats.ts
+++ hooks/usePomodoroStats.ts
@@ -9,18 +9,15 @@ import { useMemo } from "react";
 export const usePomodoroStats = (tasks: { duration: number }[]) => {
     const { numberOfPomodoro, numberOfShortBreak, numberOfLongBreak } = useMemo(() => {
         let pomodoros = 0;
-        let shortBreaks = 0;
-        let longBreaks = 0;
+
 
         tasks?.forEach((task) => {
             const pomodoroSessions = task.duration / (25 * 60);
-            const breaks = pomodoroSessions < 4 ? pomodoroSessions : 3;
-            const longBreaksCount = pomodoroSessions >= 4 ? 1 : 0;
-
             pomodoros += pomodoroSessions;
-            shortBreaks += breaks;
-            longBreaks += longBreaksCount;
         });
+
+        const shortBreaks = Math.floor(pomodoros)
+        const longBreaks = Math.floor(pomodoros / 4);
 
         return {
             numberOfPomodoro: Math.floor(pomodoros),
