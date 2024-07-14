205a798b9266319a818ccae0c28ef7710f537f4b
ThomasGaillot <thomas.gaillot@enogrid.com>
2024-07-14 16:17:28 +0200

    ui

--- app/(locale)/writing/page.tsx
+++ app/(locale)/writing/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function WritingPage() {
     return (<>
-            <div className={'hidden lg:flex flex-col justify-center items-center min-h-[80vh] gap-2'}>
+            <div className={'hidden lg:ml-80 2xl:ml-96 lg:flex flex-col justify-center items-center min-h-[80vh] gap-2'}>
                 <TypographyLead>
                     This is where you can read and write your articles.
                 </TypographyLead>
