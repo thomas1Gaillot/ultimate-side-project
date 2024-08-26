b54fd35f92e52a7f8270bdcc04c3de828dd0a89d
ThomasGaillot <thomas1.gaillot@gmail.com>
2024-08-26 14:16:05 +0200

    poc enostart

--- app/(locale)/poc-enostart/page.tsx
+++ app/(locale)/poc-enostart/page.tsx
@@ -2,7 +2,7 @@
 import {useState} from 'react'
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
 import {Button} from "@/components/ui/button"
-import {MoveRightIcon} from "lucide-react"
+import {BellIcon, HourglassIcon, MoveRightIcon} from "lucide-react"
 import {Progress} from "@/components/ui/progress";
 import {cn} from "@/lib/utils";
 
@@ -11,10 +11,10 @@ export default function Component() {
 
     const tabs = [
 
-        {id: "demarches", label: "Démarches", count: "1 /4"},
-        {id: "nouvelles-candidatures", label: "Mes Candidatures", count: 7},
-        {id: "pre-integrations", label: "Mes Pré-intégrations", count: 5},
-        {id: "passages-en-exploitation", label: "Mes Passages en exploitation", count: 2},
+        {id: "demarches", label: "Démarches", count: 3, ping: true},
+        {id: "nouvelles-candidatures", label: "Mes Candidatures", count: 7, ping: true},
+        {id: "pre-integrations", label: "Mes Pré-intégrations", count: 3, ping: false},
+        {id: "passages-en-exploitation", label: "Mes Passages en exploitation", count: 2, ping: true},
     ]
 
     const tabContents: any = {
@@ -23,9 +23,7 @@ export default function Component() {
             <>
 
                 <div className={"grid mb-2 "}>
-                    <span
-                        className={"text-xs text-gray-700"}>Il vous reste 3 démarches à faire pour créer une opération</span>
-                    <Progress className={'w-48'} label={'1/4'} value={25}/>
+                    <Progress className={'w-48'} labelLeft={'démarches effectuées'} label={'1/4'} value={25}/>
                 </div>
 
 
@@ -41,7 +39,8 @@ export default function Component() {
                         <h3 className="font-semibold  text-sm mb-2">Démarches ENEDIS</h3>
                         <ul className="space-y-1 text-sm">
                             <SmallStep label={"J'envoi la déclaration de mise en oeuvre"} done={false} index={0}/>
-                            <SmallStep label={"J'édite les accords de participation"} done={false} index={1}/>
+                            <SmallStep disabled={true} label={"J'édite les accords de participation"} done={false}
+                                       index={1}/>
                         </ul>
                     </div>
                 </div>
@@ -52,8 +51,6 @@ export default function Component() {
                 <span className={"text-xs text-gray-700"}>
                            Vous avez 7 candidatures en attente.  Vérifiez le périmètre et pré-intégrez le participant.
                 </span>
-                <Progress className={'w-48'} label={'5/12'} value={60}/>
-
                 <h3 className="font-semibold text-sm mt-2">Pré-intégrer un participant</h3>
 
                 <ul className="text-sm grid  grid-cols-1">
@@ -66,35 +63,17 @@ export default function Component() {
         ),
         "pre-integrations": (
             <>
-                <span className={"text-xs text-gray-700 mb-2"}>Intégrez vos candidats en plusieurs étapes.</span>
-
+                <Progress className={'w-48'} labelLeft={'proposer un prix de vente'} label={'3/3'} value={100}/>
                 <div className={"grid gap-4 mt-2  grid-cols-1 lg:grid-cols-2"}>
                     <div>
                         <h3 className="font-semibold text-sm mb-2">{"J'étudie les données et propose un prix"}</h3>
-                        <span
-                            className={"text-xs text-gray-700"}>
-                           A jour
-                        </span>
-                        <Progress className={'w-48'} label={'5/5'} value={100}/>
 
                         <ul className="text-sm grid   grid-cols-1">
-                            <SmallStep link={'#'} label={"Je télécharge les données pour étude (4/5)"} done={false}
+                            <SmallStep link={'#'} label={"Je télécharge les données pour étude (2/3)"} done={false}
                                        index={0}/>
-                            <SmallStep label={"Je propose un prix de vente à chaque consommateur (5/5)"} done={true}
+                            <SmallStep label={"Je propose un prix de vente à chaque consommateur (3/3)"} done={true}
                                        index={1}/>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3 className="font-semibold text-sm mb-2">{"Ensuite, j'édite les contrats de vente"}</h3>
-                        <span
-                            className={"text-xs text-gray-700"}>
-                           Attendez que les 3 consommateurs acceptent le prix.
-                        </span>
-                        <Progress className={'w-48'} label={'2/5'} value={40}/>
-
-                        <ul className="text-sm grid   grid-cols-1">
-                            <SmallStep label={"Chaque consommateur accepte le prix (2/5)"} done={false} index={2}/>
-                            <SmallStep label={"J'édite les contrats de vente (2/2)"} done={true} index={3}/>
+                            <SmallStep label={"Chaque consommateur accepte le prix"} done={false} index={2}/>
                         </ul>
                     </div>
                 </div>
@@ -102,10 +81,7 @@ export default function Component() {
         ),
         "passages-en-exploitation": (
             <>
-                <span className={"text-xs text-gray-700"}>
-                    Vous avez 3 démarches à terminer et 2 consommateurs pré-intégrés en attente.
-                </span>
-                <Progress className={'w-48'} label={'1/4'} value={25}/>
+                <Progress className={'w-48'} labelLeft={'convention créée'} label={'0/2'} value={0}/>
 
                 <div className={"grid gap-4 mt-2 grid-cols-1 lg:grid-cols-2"}>
                     <div>
@@ -113,18 +89,18 @@ export default function Component() {
                         <ul className="text-sm grid grid-cols-1 ">
                             <SmallStep label={"Les démarches sont terminées (1/4)"} done={false}
                                        index={0}/>
-                            <SmallStep label={"Les pré-intégrations sont terminées (2)"} done={true}
-                                       index={1}/>
-                            <SmallStep label={"J'envoi les documents aux consommateurs pour signature"} done={false}
+                            <SmallStep label={"J'édite les contrats de vente (2/2)"} done={false} index={1}/>
+                            <SmallStep disabled={true} label={"J'envoi les documents aux consommateurs pour signature"}
+                                       done={false}
                                        index={2}/>
                         </ul>
                     </div>
                     <div>
                         <h3 className="font-semibold text-sm mb-2">{"Editer et envoyer la convention"}</h3>
                         <ul className="text-sm grid grid-cols-1 ">
-                            <SmallStep label={"Je reçois les documents signés"} done={false} index={3}/>
-                            <SmallStep label={"Je crée la convention d'ACC"} done={false} index={4}/>
-                            <SmallStep label={"J'envoi la convention à Enedis"} done={false} index={5}/>
+                            <SmallStep disabled={true} label={"Je reçois les documents signés"} done={false} index={3}/>
+                            <SmallStep disabled={true} label={"Je crée la convention d'ACC"} done={false} index={4}/>
+                            <SmallStep disabled={true} label={"J'envoi la convention à Enedis"} done={false} index={5}/>
                         </ul>
                     </div>
                 </div>
@@ -143,9 +119,14 @@ export default function Component() {
                             value={tab.id}
                             className="flex justify-between items-center w-72 px-3 py-2 text-sm"
                         >
-                            <span className="text-left truncate mr-2">{tab.label}</span>
-                            <span
-                                className="bg-background rounded-full px-2 py-1 text-xs flex-shrink-0">{tab.count}</span>
+                            <div className={"flex items-center"}>
+                                <span className="text-left truncate mr-2">{tab.label}</span>
+                                <span
+                                    className="bg-background rounded-full px-2 py-1 text-xs flex-shrink-0">{tab.count}</span>
+
+                            </div>
+                            {tab.ping ? <BellIcon className="size-4  text-primary"/> :
+                                <HourglassIcon className="size-4 text-gray-700"/>}
                         </TabsTrigger>
                     ))}
                 </TabsList>
@@ -162,14 +143,15 @@ export default function Component() {
 }
 
 
-const SmallStep = ({label, done, index, link}: {
+const SmallStep = ({label, done, index, link, disabled}: {
     label: string,
     done: boolean,
     index: number,
-    link?: string
+    link?: string,
+    disabled?: boolean
 }) => (
 
-    <Button variant={'ghost'} size={'sm'} className="flex justify-start items-center w-max">
+    <Button disabled={disabled || done} variant={'ghost'} size={'sm'} className="flex justify-start items-center w-max">
         <div
             className="mr-2 text-xs size-6 min-w-6 rounded-full flex justify-center items-center border text-gray-700">
             {index + 1}
--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -6,18 +6,29 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 import { cn } from "@/lib/utils"
 
 interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
-    label?: string; // Add a new prop for the custom text
+    label?: string; // Prop for the text on the top-right
+    labelLeft?: string; // New prop for the text on the top-left
 }
 
 const Progress = React.forwardRef<
     React.ElementRef<typeof ProgressPrimitive.Root>,
     ProgressProps
->(({ className, value, label, ...props }, ref) => (
-    <div className="flex items-center">
+>(({ className, value, label, labelLeft, ...props }, ref) => (
+    <div className="flex flex-col w-max py-1">
+        <div className="flex justify-between mb-0.5">
+                <span className="text-xs text-gray-700">
+                    {labelLeft || ''}
+                </span>
+            {label && (
+                <span className="text-xs text-gray-700 tracking-wider">
+                    {label}
+                </span>
+            )}
+        </div>
         <ProgressPrimitive.Root
             ref={ref}
             className={cn(
-                "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+                "relative h-1.5 w-full overflow-hidden rounded-full bg-primary/20",
                 className
             )}
             {...props}
@@ -27,11 +38,6 @@ const Progress = React.forwardRef<
                 style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
             />
         </ProgressPrimitive.Root>
-        {label && (
-            <span className="ml-2 text-xs font-medium text-gray-700">
-        {label}
-      </span>
-        )}
     </div>
 ))
 Progress.displayName = ProgressPrimitive.Root.displayName
